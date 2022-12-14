276f6354183d7d6487da56251320928007ad7f37
Kacper Weron <weron.kacper@gmail.com>
2022-12-14 19:20:42 +0100

    Changes as recommended

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@
           console.log("Hello World");
      };
 
-     welcome();
+     
 
-     const calculate = (ammount, currency) => {
+     const calculateAmountInPln = (ammount, currency) => {
 
           const rateUSD = 4.48;
           const ratePLN = 1;
@@ -27,7 +27,7 @@
           };
      }
 
-     const calculateResult = (resultValue, result) => {
+     const calculateResult = (resultValue, amountInPln) => {
 
           const rateUSD = 4.48;
           const ratePLN = 1;
@@ -36,17 +36,23 @@
 
           switch (resultValue) {
                case "resultUSD":
-                    return result / rateUSD;
+                    return amountInPln / rateUSD;
 
                case "resultPLN":
-                    return result / ratePLN;
+                    return amountInPln / ratePLN;
 
                case "resultARS":
-                    return result / rateARS;
+                    return amountInPln / rateARS;
 
                case "resultEUR":
-                    return result / rateEUR;
+                    return amountInPln / rateEUR;
           };
+     }
+
+     const updateResultText = (outcome) => {
+           const resultElement = document.querySelector(".js-result");
+           
+           resultElement.value = `${outcome.toFixed(2)}`;
      }
 
      const onFormSubmit = (event) => {
@@ -54,20 +60,22 @@
 
           const currencyElement = document.querySelector(".js-currency");
           const ammountElement = document.querySelector(".js-ammount");
-          const resultElement = document.querySelector(".js-result");
           const resultValueElement = document.querySelector(".js-resultValue");
 
           let currency = currencyElement.value;
           let ammount = ammountElement.value;
-          const result = calculate(ammount, currency);
+          const amountInPln = calculateAmountInPln(ammount, currency);
           const resultValue = resultValueElement.value;
-          const outcome = calculateResult(resultValue, result);
-
-          resultElement.value = `${outcome.toFixed(2)}`;
+          const outcome = calculateResult(resultValue, amountInPln);
+          
+          updateResultText(outcome);
+          
      };
 
      const init = () => {
           const formElement = document.querySelector(".js-form");
+
+          welcome();
 
           formElement.addEventListener("submit", onFormSubmit);
      };
